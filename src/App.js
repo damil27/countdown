2091ce2803365d3700a7b242718c40f47e3c075f
damin <34189426+damil27@users.noreply.github.com>
2023-06-30 09:00:03 +0100

    modification of countdown timer

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import { Countdown } from "./components/Countdown";
 import { Footer } from "./components/Footer";
 import { Social } from "./components/Social";
 
-
 function App() {
   return (
     <div style={{ backgroundImage: "url(./assets/images/bg-stars.svg)" }}>
       <div className=" min-h-screen relative overflow-hidden flex items-center justify-center font-Red_hat">
-        <Countdown future="Fri Jun 30 2023 08:50:23" />
-       
+        <Countdown future="Fri Jul 30 2023 08:50:23" />
       </div>
       <Social />
       <Footer />
